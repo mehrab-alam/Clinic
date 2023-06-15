f13524997ac519433f4c3079f6df01715ff65986
Mehrab <mehrab2000alam@gmail.com>
2023-06-15 11:16:20 -0400

    changed the table folder

--- table/index.js
+++ table/index.js
@@ -1,5 +1,11 @@
 import mysql from 'mysql2'
 
+console.log(DB_DATABASE)
+console.log(DB_HOST)
+console.log(DB_PASSWORD)
+console.log(DB_USERS)
+
+
 const connection = mysql.createConnection({
     user: process.env.DB_USERS,
     password: process.env.DB_PASSWORD,
