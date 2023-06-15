4ed2d383af009df434a6ba37af3cbda1b1053996
Mehrab <mehrab2000alam@gmail.com>
2023-06-15 11:35:43 -0400

    added new table folder

---
+++ table_1/index.js
@@ -0,0 +1,24 @@
+import mysql from 'mysql2'
+
+
+
+
+const connection = mysql.createConnection({
+    user: process.env.DB_USERS,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_DATABASE
+});
+connection.connect((err) => {
+    if (err) {
+        console.log(err)
+
+    }
+    const table = "create table patients(patientId SMALLINT, name varchar(25),mail varchar(40),number INT,Date VARCHAR(40),time VARCHAR(40),location VARCHAR(20) ,address VARCHAR(50));"
+    connection.query(table, function (err, result) {
+        if (err) throw err;
+
+    })
+
+})
